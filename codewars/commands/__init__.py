2a5512c79654d801b33f78446d3bfbea4b24f620
Erik Parreira <etinlb@gmail.com>
2016-11-26 12:34:15 -0800

    Moved sublime commands to a seperate command folder
    Top level code is now just reponsible for loading the commands and nothing else

---
+++ codewars/commands/__init__.py
@@ -0,0 +1,7 @@
+"""
+Need to load all the commands when package is imported in order for sublime to
+register all the commands
+"""
+from .insert_kata import InsertProblemViewCommand, InsertTestFixtureCommand
+from .start_kata import StartKataCommand
+
