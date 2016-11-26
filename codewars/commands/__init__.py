42336be791b41d7bb00933ebcf6bbeb7a974f2b8
Erik Parreira <etinlb@gmail.com>
2016-11-26 12:37:26 -0800

    Added submit command

--- codewars/commands/__init__.py
+++ codewars/commands/__init__.py
@@ -4,4 +4,4 @@ register all the commands
 """
 from .insert_kata import InsertProblemViewCommand, InsertTestFixtureCommand
 from .start_kata import StartKataCommand
-
+from .submit_kata import SubmitProblemCommand
